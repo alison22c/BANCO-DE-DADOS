0f747a70658733a963a2000c3884dce94b7ed291
Alison <143205053+alison22c@users.noreply.github.com>
2024-05-30 18:27:47 -0300

    trabalho finalizado da  list1_2bimes.sql

--- 2-bimestre/list1_2bimes.sql
+++ 2-bimestre/list1_2bimes.sql
@@ -1,4 +1,5 @@
-use avaliacao_22c;
+create database avaliacao_22C;
+use avaliacao_22C;
 
 SELECT * FROM livros;
 
@@ -26,5 +27,56 @@ where ano_publicacao <= 2000;
 
 update livros
 set editora=false
-where ano_publicacao <= 2000;    
+where ano_publicacao <= 2000;   
 
+UPDATE livros
+SET titulo = 'Harry Potter e a Pedra Filosofal (Edição Especial)'
+WHERE isbn = '978-0439708180';
+ 
+ DELETE FROM livros
+WHERE categoria = 'Terror';
+
+DELETE FROM livros
+WHERE idioma = 'Francês' AND ano_publicacao < 1970;
+
+DELETE FROM livros
+WHERE titulo = 'As Crônicas de Nárnia';
+
+DELETE FROM livros
+WHERE editora = 'Penguin Books';
+
+SELECT *
+FROM livros
+WHERE quantidade_paginas > 500;
+
+SELECT categoria, COUNT(*) AS quantidade
+FROM livros
+GROUP BY categoria;
+
+SELECT COUNT(*) AS total_livros, AVG(quantidade_paginas) AS media_paginas
+FROM livros
+WHERE categoria = 'Drama';
+
+SELECT AVG(ano_publicacao) AS media_ano_publicacao
+FROM livros;
+
+SELECT *
+FROM livros
+WHERE ano_publicacao = (SELECT MAX(ano_publicacao) FROM livros)
+OR ano_publicacao = (SELECT MIN(ano_publicacao) FROM livros);
+
+SELECT *
+FROM livros
+ORDER BY ano_publicacao DESC;
+
+SELECT titulo
+FROM livros
+WHERE idioma = 'Inglês'
+UNION
+SELECT titulo
+FROM livros
+WHERE idioma = 'Português';
+
+SELECT *
+FROM livros
+WHERE autor = 'George Orwell';
